da499e1fb63bf7561421742590eb03ad15d0749e
Carlos Razo <razo8a@hotmail.com>
2022-05-07 19:29:28 -0500

    added connection to postgres db scripts

---
+++ load_csv_files.py
@@ -0,0 +1,30 @@
+import csv
+
+
+def read_departaments():
+    with open('raw_data/departments.csv', newline='') as f:
+        reader_dep = csv.reader(f)
+        for row in reader_dep:
+            print(row)
+
+def read_hired_employees():
+    with open('raw_data/hired_employees.csv', newline='') as f:
+        reader_dep = csv.reader(f)
+        for row in reader_dep:
+            print(row)
+
+def read_jobs():
+    with open('raw_data/jobs.csv', newline='') as f:
+        reader_dep = csv.reader(f)
+        for row in reader_dep:
+            print(row)
+
+def load_csv_to_postgres():
+    pass
+
+
+if __name__ == '__main__':
+    #read_departaments()
+    read_jobs()
+
+
