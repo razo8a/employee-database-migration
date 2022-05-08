cd65c85d6fd04081156508b0ccf1ab374ff2ff80
Omar Aguayo <omaraguayo@gmail.com>
2022-05-08 12:31:19 -0500

    les quite el (raw_data/) por que no me corria

--- raw_data/Global_proyect.py
+++ raw_data/Global_proyect.py
@@ -1,21 +1,32 @@
 import csv
 
+
 def read_departaments():
     with open('departments.csv', newline='') as f:
-        reader_dep = csv.reader(f, delimiter=' ', quotechar='|')
+        reader_dep = csv.reader(f)
         for row in reader_dep:
-            print(', '.join(row))
-    print(reader_dep)
+            print(row)
 
-def read_hird_employees():
-    pass
+def read_hired_employees():
+    with open('hired_employees.csv', newline='') as f:
+        reader_dep = csv.reader(f)
+        for row in reader_dep:
+            print(row)
 
-def read_jods():
+def read_jobs():
+    with open('jobs.csv', newline='') as f:
+        reader_dep = csv.reader(f)
+        for row in reader_dep:
+            print(row)
+
+def load_csv_to_postgres():
     pass
 
 
 if __name__ == '__main__':
-    pass
+    read_hired_employees()
+    #read_departaments()
+    #read_jobs()
 
 
 
