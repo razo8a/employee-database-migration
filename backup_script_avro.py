a60daa27c7f617e4a8ffc39fa1d02a7896aa922a
Omar Aguayo <omar.aguayo14@gmail.com>
2022-07-14 21:29:30 -0500

    Add script for backup and this script create a AvroFile

--- backup_script_avro.py
+++ backup_script_avro.py
@@ -5,7 +5,7 @@ from fastavro import writer, reader, parse_schema
 
 def main():
     # Create the SQL engine object
-    alchemyEngine = create_engine('postgresql+psycopg2://carlos_razo:@localhost/mydatabase')
+    alchemyEngine = create_engine('postgresql+psycopg2://omaraguayo:IslaCartagena@localhost/employees')
 
     # Connect to Postgres Database
     ps_conn = alchemyEngine.connect()
@@ -18,6 +18,28 @@ def main():
 
     # Close the connection to database
     ps_conn.close()
+
+    schema = {
+        'doc': 'Data_base1',
+        'name': 'Employee',
+        'namespace': 'in',
+        'type': 'record',
+        'fields': [
+            {'name': 'employee_id', 'type': 'int'},
+            {'name': 'first_name', 'type': 'string'},
+            {'name': 'last_name', 'type': 'string'},
+            {'name': 'email', 'type': 'string'},
+            {'name': 'crated_on', 'type': ["null", "int"]}
+        ]
+    }
+    _parse_schema = parse_schema(schema)
+
+    # covert pd.DataFrame to records - list of dictionaries
+    records = df.to_dict('records')
+
+    # Write to Avro file
+    with open('backup.avro', 'wb') as out:
+        writer(out, _parse_schema, records)
 
 
 if __name__ == '__main__':
